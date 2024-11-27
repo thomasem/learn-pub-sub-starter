5c4e52322e82f5c0c6a0a752e72ab00c9d0349fa
Thomas Maddox <me@thomasem.net>
2024-11-27 08:45:54 -0600

    More multi-day friendly rabbit.sh and sends pause message

--- rabbit.sh
+++ rabbit.sh
@@ -3,11 +3,19 @@
 case "$1" in
     start)
         echo "Starting RabbitMQ container..."
-        docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+        if docker inspect rabbitmq &>/dev/null; then
+            docker start rabbitmq
+        else
+            docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+        fi
         ;;
     stop)
         echo "Stopping RabbitMQ container..."
         docker stop rabbitmq
+        ;;
+    delete)
+        echo "Deleting RabbitMQ container..."
+        docker rm -fv rabbitmq
         ;;
     logs)
         echo "Fetching logs for RabbitMQ container..."

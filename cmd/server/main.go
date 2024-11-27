18560b44b349157b9186c9e204d0f101371b2179
Thomas Maddox <me@thomasem.net>
2024-11-27 08:45:54 -0600

    More multi-day friendly rabbit.sh and sends pause message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	fmt.Println("Startng Peril server...")
+
+	url := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		conn.Close()
+		fmt.Println("Closed RabbitMQ connection.")
+	}()
+
+	fmt.Println("Connected to RabbitMQ. Press Ctrl + C to exit.")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println("Error opening channel:", err)
+		os.Exit(1)
+	}
+
+	err = pubsub.PublishJSON(
+		ch,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: true},
+	)
+	if err != nil {
+		fmt.Println("Error sending pubsub message:", err)
+		os.Exit(1)
+	}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt)
+	for range sc {
+		fmt.Println("\nInterrupt detected. Exiting...")
+		break
+	}
 }
